5a814b3b71cea8360e1d0c99767aae9db4800f01
vogaro-kanno <k.kanno@vogaro.co.jp>
2025-06-27 18:42:38 +0900

    cssをsrc内に生成しないようdistを作成

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,35 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        // 依存関係の警告を抑制
+        quietDeps: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    cssCodeSplit: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name?.split('.') || [];
+          const extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType || '')) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(extType || '')) {
+            return `assets/css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  }
 })
