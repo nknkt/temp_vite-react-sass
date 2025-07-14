fa01145383b67aa348e8d3e34a7af8cccb5db2cd
vogaro-kanno <k.kanno@vogaro.co.jp>
2025-07-14 18:06:33 +0900

    global→style.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './assets/styles/global.scss';
+import './assets/styles/style.scss';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
